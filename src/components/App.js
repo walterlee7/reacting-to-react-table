7b01876da35e910691dcd64fafb251554ab3ff95
walterlee <walter.lee9@gmail.com>
2019-07-12 14:43:53 -0500

    added react-table and example

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        const data = [{
+            name: 'Roy Agasthyan',
+            age: 26
+        }, {
+            name: 'Sam Thomason',
+            age: 22
+        }, {
+            name: 'Michael Jackson',
+            age: 36
+        }, {
+            name: 'Samuel Roy',
+            age: 56
+        }, {
+            name: 'Rima Soy',
+            age: 28
+        }, {
+            name: 'Suzi Eliamma',
+            age: 28
+        }]
+
+        const columns = [{
+            Header: 'Name',
+            accessor: 'name'
+        }, {
+            Header: 'Age',
+            accessor: 'age'
+        }]
+
+        return (
+            <div>
+                <ReactTable
+                    data={data}
+                    columns={columns}
+                    defaultPageSize={3}
+                    pageSizeOptions={[3, 6]}
+                />
+            </div>
+        )
+
+    }
 }
 
 export default App;
